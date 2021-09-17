20c9c5c0ac951a2be6fb4edc4cc342dbbce9b91d
Neil <74466880+spidernh@users.noreply.github.com>
2021-09-17 17:46:27 -0400

    Create AutonomousTemplate.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousTemplate.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Utils.Hardware;
+
+@Autonomous(name = "Autonomous Template", group = "Linear Opmode")
+@Disabled
+public class AutonomousTemplate extends LinearOpMode {
+
+	private String versionNumber = "v0.1";
+	private Hardware robot = new Hardware(this, telemetry);
+
+	@Override
+	public void runOpMode() {
+		robot.init(hardwareMap);
+
+		telemetry.addData("Status", "Initialized (Version: " + versionNumber + ")");
+		telemetry.update();
+
+		waitForStart();
+		if (!opModeIsActive()) return;
+
+		telemetry.addData("Status", "Started (Version: " + versionNumber + ")");
+
+		robot.resetAngle();
+
+		// Autonomous goes here
+
+		telemetry.addData("Status", "Stopped (Version: " + versionNumber + ")");
+		telemetry.update();
+	}
+}
