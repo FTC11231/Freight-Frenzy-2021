219a01a2cf5fce9955c81ff06abca21d2ae4eabf
Neil <74466880+spidernh@users.noreply.github.com>
2021-10-10 02:53:11 -0400

    Added displacement markers in the MeepMeep test

--- MeepMeepTesting/src/main/java/com/ftc11231/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/ftc11231/meepmeeptesting/MeepMeepTesting.java
@@ -3,7 +3,7 @@ package com.ftc11231.meepmeeptesting;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
-import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueLight;
 
 public class MeepMeepTesting {
 	public static void main(String[] args) {
@@ -16,7 +16,7 @@ public class MeepMeepTesting {
 				// Set field image
 				.setBackground(MeepMeep.Background.FIELD_FREIGHT_FRENZY)
 				// Set theme
-				.setTheme(new ColorSchemeRedDark())
+				.setTheme(new ColorSchemeBlueLight())
 				// Background opacity from 0-1
 				.setBackgroundAlpha(1f)
 				// Set constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
@@ -25,24 +25,37 @@ public class MeepMeepTesting {
 						drive.trajectorySequenceBuilder(new Pose2d(-38, -63, Math.toRadians(90)))
 								.setReversed(false)
 								.splineTo(new Vector2d(-26, -35), Math.toRadians(38))
-								.waitSeconds(0.25)
+								.waitSeconds(0.1)
+								.addDisplacementMarker(() -> {
+									// Deposit preload box
+								})
+								.waitSeconds(0.8)
 								.setReversed(true)
 								.splineTo(new Vector2d(-54, -54), Math.toRadians(225))
-								.waitSeconds(0.3)
+								.waitSeconds(0.1)
+								.addDisplacementMarker(() -> {
+									// Start turning carousel
+								})
+								.waitSeconds(2)
+								.addDisplacementMarker(() -> {
+									// Stop turning carousel
+								})
 								.setReversed(false)
 								.splineTo(new Vector2d(-10, -45), Math.toRadians(0))
 								.splineTo(new Vector2d(10, -40), Math.toRadians(0))
-								.waitSeconds(0.3)
 								.splineTo(new Vector2d(45, -40), Math.toRadians(0))
 								.splineTo(new Vector2d(58, -39), Math.toRadians(0))
-								.waitSeconds(5)
 								.build()
 				)
 //				.followTrajectorySequence(drive ->
 //						drive.trajectorySequenceBuilder(new Pose2d(9, -63, Math.toRadians(90)))
 //								.setReversed(false)
 //								.splineTo(new Vector2d(2, -40), Math.toRadians(135))
-//								.waitSeconds(0.25)
+//								.waitSeconds(0.1)
+//								.addDisplacementMarker(() -> {
+//									// Deposit preload box
+//								})
+//								.waitSeconds(0.8)
 //								.setReversed(true)
 //								.splineTo(new Vector2d(10, -44), Math.toRadians(0))
 //								.waitSeconds(0.1)
