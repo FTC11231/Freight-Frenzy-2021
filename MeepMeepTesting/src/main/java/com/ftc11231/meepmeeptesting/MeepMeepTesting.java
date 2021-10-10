487cf09e922c0af5b31575cccb47a3f062e62455
Neil <74466880+spidernh@users.noreply.github.com>
2021-10-10 16:28:38 -0400

    Changed to more displacement markers
    Or maybe not, the last commit didn't have any temporal markers so maybe I didn't ever use them. Anyways there are more markers now.

--- MeepMeepTesting/src/main/java/com/ftc11231/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/ftc11231/meepmeeptesting/MeepMeepTesting.java
@@ -21,48 +21,60 @@ public class MeepMeepTesting {
 				.setBackgroundAlpha(1f)
 				// Set constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
 				.setConstraints(50, 40, Math.toRadians(180), Math.toRadians(180), 15)
-				.followTrajectorySequence(drive ->
-						drive.trajectorySequenceBuilder(new Pose2d(-38, -63, Math.toRadians(90)))
-								.setReversed(false)
-								.splineTo(new Vector2d(-26, -35), Math.toRadians(38))
-								.waitSeconds(0.1)
-								.addDisplacementMarker(() -> {
-									// Deposit preload box
-								})
-								.waitSeconds(0.8)
-								.setReversed(true)
-								.splineTo(new Vector2d(-54, -54), Math.toRadians(225))
-								.waitSeconds(0.1)
-								.addDisplacementMarker(() -> {
-									// Start turning carousel
-								})
-								.waitSeconds(2)
-								.addDisplacementMarker(() -> {
-									// Stop turning carousel
-								})
-								.setReversed(false)
-								.splineTo(new Vector2d(-10, -45), Math.toRadians(0))
-								.splineTo(new Vector2d(10, -40), Math.toRadians(0))
-								.splineTo(new Vector2d(45, -40), Math.toRadians(0))
-								.splineTo(new Vector2d(58, -39), Math.toRadians(0))
-								.build()
-				)
 //				.followTrajectorySequence(drive ->
-//						drive.trajectorySequenceBuilder(new Pose2d(9, -63, Math.toRadians(90)))
+//						drive.trajectorySequenceBuilder(new Pose2d(-38, -63, Math.toRadians(90)))
+//								.addDisplacementMarker(2, () -> {
+//									// Turn turret and raise/lower arm
+//								})
 //								.setReversed(false)
-//								.splineTo(new Vector2d(2, -40), Math.toRadians(135))
+//								.splineTo(new Vector2d(-26, -35), Math.toRadians(38))
 //								.waitSeconds(0.1)
 //								.addDisplacementMarker(() -> {
 //									// Deposit preload box
 //								})
+//								.addDisplacementMarker(42, () -> {
+//									// Lower arm and turn turret to a neutral position
+//								})
 //								.waitSeconds(0.8)
 //								.setReversed(true)
-//								.splineTo(new Vector2d(10, -44), Math.toRadians(0))
+//								.splineTo(new Vector2d(-54, -54), Math.toRadians(225))
 //								.waitSeconds(0.1)
-//								.splineTo(new Vector2d(38, -44), Math.toRadians(0))
-//								.splineTo(new Vector2d(38, -61), Math.toRadians(270))
+//								.addDisplacementMarker(() -> {
+//									 // Start turning carousel
+//								})
+//								.waitSeconds(2)
+//								.addDisplacementMarker(60, () -> {
+//									 // Stop turning carousel
+//								})
+//								.setReversed(false)
+//								.splineTo(new Vector2d(-10, -45), Math.toRadians(0))
+//								.splineTo(new Vector2d(10, -40), Math.toRadians(0))
+//								.splineTo(new Vector2d(45, -40), Math.toRadians(0))
+//								.splineTo(new Vector2d(58, -39), Math.toRadians(0))
 //								.build()
 //				)
+				.followTrajectorySequence(drive ->
+						drive.trajectorySequenceBuilder(new Pose2d(9, -63, Math.toRadians(90)))
+								.addDisplacementMarker(2, () -> {
+									// Turn turret and raise/lower arm
+								})
+								.setReversed(false)
+								.splineTo(new Vector2d(2, -40), Math.toRadians(135))
+								.waitSeconds(0.1)
+								.addDisplacementMarker(() -> {
+									// Deposit preload box
+								})
+								.addDisplacementMarker(30, () -> {
+									// Move turret and arm to netural position
+								})
+								.waitSeconds(0.8)
+								.setReversed(true)
+								.splineTo(new Vector2d(10, -44), Math.toRadians(0))
+								.waitSeconds(0.1)
+								.splineTo(new Vector2d(38, -44), Math.toRadians(0))
+								.splineTo(new Vector2d(38, -61), Math.toRadians(270))
+								.build()
+				)
 				.start();
 	}
 }
